48970038a8863fb2bc699fef6a358312c7f044fd
sho-hama <shokihamana@gmail.com>
2021-03-15 00:47:06 +0900

    shebang追加

--- volume/ros2java_ws/install_ros2java.sh
+++ volume/ros2java_ws/install_ros2java.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -eu
 vcs import src < ros2_java_desktop.repos
 sed -ie 's/client\.waitForService()/true/' ./src/ros2_java/ros2_java_examples/rcljava_examples/src/main/java/org/ros2/rcljava/examples/client/AddTwoIntsClient.java
